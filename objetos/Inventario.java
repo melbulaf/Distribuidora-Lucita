88d12a07d27b4ac42fc8aaf7bb745d4cf9771551
sacatanoc <sacatanoc@unal.edu.co>
2025-06-15 20:50:16 -0500

    Add files via upload

---
+++ objetos/Inventario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquete_de_fuentes;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Inventario {
+    
+    public static ArrayList<Producto> arregloInventario = new ArrayList<>();
+    
+}

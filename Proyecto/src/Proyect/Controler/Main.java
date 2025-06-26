32fc4465487b98506ce1f3b8f29b037788827c8f
EmmanuelBetancur <ebetancuru@unal.edu.co>
2025-06-25 22:46:35 -0500

    Frames Actualizados

--- Proyecto/src/Proyect/Controler/Main.java
+++ Proyecto/src/Proyect/Controler/Main.java
@@ -24,9 +24,11 @@ public class Main {
         }
         // Mostrar la ventana
         java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Inicio().setVisible(true);
-            }
-        });
-    }
+        public void run() {
+            Inicio inicio = new Inicio();
+            inicio.setSize(1000,700);
+            inicio.setVisible(true);
+        }
+    });
+    } 
 }

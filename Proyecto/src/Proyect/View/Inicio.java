448f2f2a36731f4a7bc5da5b1ab10124721a1b65
jmoreano574 <jmoreano@unal.edu.co>
2025-07-03 13:02:23 -0500

    cy

--- Proyecto/src/Proyect/View/Inicio.java
+++ Proyecto/src/Proyect/View/Inicio.java
@@ -138,6 +138,7 @@ public class Inicio extends javax.swing.JFrame {
         menuItemNuevoProducto = new javax.swing.JMenuItem();
         MEditar = new javax.swing.JMenuItem();
         menuItemRutaDelDia = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
         salirMenuItem = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -283,6 +284,14 @@ public class Inicio extends javax.swing.JFrame {
             }
         });
         fileMenu.add(menuItemRutaDelDia);
+
+        jMenuItem2.setText("Resumen de ganancias");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        fileMenu.add(jMenuItem2);
 
         salirMenuItem.setMnemonic('x');
         salirMenuItem.setText("Salir");
@@ -435,6 +444,14 @@ public class Inicio extends javax.swing.JFrame {
             }
     }//GEN-LAST:event_menuItemRutaDelDiaActionPerformed
 
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+        FromResumenDeGanancias a = new FromResumenDeGanancias();
+        desktopPane.add(a);
+        a.setVisible(true);
+        
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -444,6 +461,7 @@ public class Inicio extends javax.swing.JFrame {
     private javax.swing.JDesktopPane desktopPane;
     private javax.swing.JMenu fileMenu;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JInternalFrame jifFormEditar;
     private javax.swing.JInternalFrame jifFormHCompras;
     private javax.swing.JInternalFrame jifFormInventario;
